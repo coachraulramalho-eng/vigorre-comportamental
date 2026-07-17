c30b9e36e4c7fb1fd524300038396976bbb973c2
coachraulramalho-eng <coach.raulramalho@gmail.com>
2026-07-17 10:08:00 -0300

    Create index.js

---
+++ algorithms/index.js
@@ -0,0 +1,17 @@
+const disc = require('./disc');
+const bigfive = require('./bigfive');
+const ie = require('./ie');
+const valores = require('./valores');
+const swot = require('./swot');
+const vigor = require('./vigor');
+const matching = require('./matching');
+
+module.exports = {
+  disc,
+  bigfive,
+  ie,
+  valores,
+  swot,
+  vigor,
+  matching
+};
