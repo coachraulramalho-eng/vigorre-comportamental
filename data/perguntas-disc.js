ee7db8b6b44fa945e9491ac679222f94c9edacff
coachraulramalho-eng <coach.raulramalho@gmail.com>
2026-07-17 10:35:24 -0300

    Add perguntasDISC data structure with questions

---
+++ data/perguntas-disc.js
@@ -0,0 +1,17 @@
+export const perguntasDISC = [
+    {
+        enunciado: 'Em uma reunião, você:',
+        opcaoA: 'Defende suas ideias com convicção',
+        opcaoB: 'Busca consenso antes de falar',
+        dimensaoA: 'D',
+        dimensaoB: 'I'
+    },
+    {
+        enunciado: 'Ao receber uma tarefa importante, você:',
+        opcaoA: 'Assume o controle e decide como fazer',
+        opcaoB: 'Pergunta como os outros gostariam que fosse feito',
+        dimensaoA: 'D',
+        dimensaoB: 'S'
+    },
+    // ... Continue com todas as 28 perguntas do PRD
+];
