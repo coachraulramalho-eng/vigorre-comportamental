3f0d411d7105dd576d6d21cba24a4a0aeb406e24
coachraulramalho-eng <coach.raulramalho@gmail.com>
2026-07-17 10:11:57 -0300

    Create constants.js

---
+++ algorithms/vigor/constants.js
@@ -0,0 +1,9 @@
+const VIGOR_WEIGHTS = {
+  visao_estrategica: { weight: 0.20 },
+  inteligencia_humana: { weight: 0.25 },
+  gestao_performance: { weight: 0.20 },
+  organizacao_estrutural: { weight: 0.15 },
+  resultados_sustentaveis: { weight: 0.20 }
+};
+
+module.exports = { VIGOR_WEIGHTS };
